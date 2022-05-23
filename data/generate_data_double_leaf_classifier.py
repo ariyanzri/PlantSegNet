6d4c6ab62b71a5908b27312d9539e0bb51898112
ariyanzri <ariyanzarei@email.arizona.edu>
2022-05-23 15:23:57 -0700

    final updates before leave

--- data/generate_data_double_leaf_classifier.py
+++ data/generate_data_double_leaf_classifier.py
@@ -1,4 +1,5 @@
 from turtle import position
+from unittest import result
 import matplotlib.pyplot as plt
 import torch
 import os
@@ -6,6 +7,10 @@ import pandas as pd
 import random
 import numpy as np
 from scipy.spatial.distance import cdist
+import sys
+sys.path.append("..")
+from data.load_raw_data import load_pcd_plyfile
+import open3d as o3d
 
 '''
 algorithm:
@@ -86,9 +91,24 @@ def calculate_leaf_tip_distances(positions):
     distances = cdist(positions,positions)
     return distances
 
-def get_leaves_with_close_distance(distances,pcd_path):
+def get_leaves_with_close_distance(distances,pcd_path,result_path,pcd_name):
     x,y = np.where((distances>0)&(distances<0.1))
-    print(x,y)
+    list_leaf_indices = list(zip(x,y))
+    unique_list_leaf_indices = []
+    for l1,l2 in list_leaf_indices:
+        if (l2,l1) not in unique_list_leaf_indices and (l1,l2) not in unique_list_leaf_indices and l1!=0 and l2!=0:
+            unique_list_leaf_indices.append((l1,l2))
+    
+    pcd = load_pcd_plyfile(os.path.join(pcd_path,pcd_name+".ply"))
+    leaf_ind = pcd['leaf_index']
+    points = pcd['points']
+    plant_ind = pcd['is_focal_plant']
+
+    for l1,l2 in unique_list_leaf_indices:
+        new_points = points[(leaf_ind==l1)|(leaf_ind==l2)&(plant_ind==1)]
+        new_pcd = o3d.geometry.PointCloud()
+        new_pcd.points = o3d.utility.Vector3dVector(new_points)    
+        o3d.io.write_point_cloud(os.path.join(result_path,f"test_{l1}_{l2}_{pcd_name}.ply"),new_pcd)
 
 def generate_data(dataset_path):
     pcd_path = os.path.join(dataset_path,"PointCloud")
@@ -104,10 +124,10 @@ def generate_data(dataset_path):
         positions = np.stack((x,y,z),axis=-1)
         distances = calculate_leaf_tip_distances(positions)
         
-        get_leaves_with_close_distance(distances,pcd_path)
+        get_leaves_with_close_distance(distances,pcd_path,"/space/ariyanzarei/sorghum_segmentation/results",pcd_f.replace(".ply",""))
 
     # points, ground, semantic, plant, leaf = ds[4]
     # single,double = generate_data_single_pcd(points,ground,plant,leaf)
     # print(len(single),len(double))
 
-generate_data("/space/ariyanzarei/sorghum_segmentation/dataset/2022-05-01")
+generate_data("/space/ariyanzarei/sorghum_segmentation/dataset/2022-05-06")
