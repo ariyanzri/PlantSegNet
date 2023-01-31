7908af8b7f7e5d127021f5f7fa59f73d98d39fa0
ariyanzri <ariyanzarei@email.arizona.edu>
2023-01-30 17:26:39 -0700

    updating KNN module. Errors fixed and code is working. 3d color example worked.

--- models/modules.py
+++ models/modules.py
@@ -4,14 +4,13 @@ import torch.nn.functional as F
 
 
 class KNNSpaceMean(nn.Module):
-    def __init__(self):
+    def __init__(self, max_k):
         super(KNNSpaceMean, self).__init__()
-        self.k_vector = torch.nn.Parameter(torch.randn(2))
+        self.k_vector = torch.nn.Parameter(torch.randn(max_k))
         self.k_vector.requires_grad = True
 
     def forward(self, points, preds):
         k = torch.argmax(F.softmax(self.k_vector, 0), dim=0) + 1
-        k = 2
         distance = torch.cdist(points, points)
         k_nearest_ind = torch.topk(distance, k, 2, False)[1]
         k_nearest_ind = k_nearest_ind.unsqueeze(-1).expand(
@@ -27,19 +26,19 @@ class KNNSpaceMean(nn.Module):
 
 import matplotlib.pyplot as plt
 
-knn_module = KNNSpaceMean()
+knn_module = KNNSpaceMean(10)
 
 points = torch.rand((4, 8000, 3))
 preds = torch.rand((4, 8000, 256))
 
-# points = torch.rand((1, 10, 3))
-# preds = torch.rand((1, 10, 3))
+points = torch.rand((1, 10, 3))
+preds = torch.rand((1, 10, 3))
 
 new_space = knn_module(points, preds)
 
-# fig = plt.figure(figsize=(20, 20))
-# ax1 = fig.add_subplot(1, 2, 1, projection="3d")
-# ax2 = fig.add_subplot(1, 2, 2, projection="3d")
-# ax1.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=preds.squeeze())
-# ax2.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=new_space.squeeze())
-# plt.show()
+fig = plt.figure(figsize=(20, 20))
+ax1 = fig.add_subplot(1, 2, 1, projection="3d")
+ax2 = fig.add_subplot(1, 2, 2, projection="3d")
+ax1.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=preds.squeeze())
+ax2.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=new_space.squeeze())
+plt.show()
