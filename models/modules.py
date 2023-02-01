8c535b1348831756a0045a0a5313c26e7831df1e
ariyanzri <ariyanzarei@email.arizona.edu>
2023-02-01 15:51:34 -0700

    updating moduels

--- models/modules.py
+++ models/modules.py
@@ -12,15 +12,21 @@ class KNNSpaceMean(nn.Module):
     def forward(self, points, preds):
         k = torch.argmax(F.softmax(self.k_vector, 0), dim=0) + 1
         distance = torch.cdist(points, points)
-        k_nearest_ind = torch.topk(distance, k, 2, False)[1]
-        k_nearest_ind = k_nearest_ind.unsqueeze(-1).expand(
-            *k_nearest_ind.shape, preds.shape[2]
+        k_nearest_ind = (
+            (torch.topk(distance, k, 2, False)[1])
+            .unsqueeze(-1)
+            .expand(-1, -1, -1, preds.shape[2])
         )
-        preds = preds.unsqueeze(1).expand(
-            (preds.shape[0], preds.shape[1], preds.shape[1], preds.shape[2])
+        new_space = torch.mean(
+            (
+                preds.unsqueeze(1)
+                .expand(
+                    (preds.shape[0], preds.shape[1], preds.shape[1], preds.shape[2])
+                )
+                .gather(2, k_nearest_ind)
+            ),
+            2,
         )
-        knn_preds = preds.gather(2, k_nearest_ind)
-        new_space = torch.mean(knn_preds, 2)
         return new_space
         # k = torch.argmax(F.softmax(self.k_vector, 0), dim=0) + 1
         # x = torch.cdist(points, points)
@@ -34,25 +40,25 @@ class KNNSpaceMean(nn.Module):
         # return preds
 
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# knn_module = KNNSpaceMean(10).cuda()
+knn_module = KNNSpaceMean(10).to(torch.device("cuda"))
 
-# points = torch.rand((4, 8000, 3)).cuda()
-# preds = torch.rand((4, 8000, 256)).cuda()
+# points = torch.rand((4, 8000, 3)).to(torch.device("cuda"))
+# preds = torch.rand((4, 8000, 256)).to(torch.device("cuda"))
 
-# # points = torch.rand((1, 10, 3)).cuda()
-# # preds = torch.rand((1, 10, 3)).cuda()
+points = torch.rand((1, 10, 3)).to(torch.device("cuda"))
+preds = torch.rand((1, 10, 3)).to(torch.device("cuda"))
 
-# new_space = knn_module(points, preds)
+new_space = knn_module(points, preds)
 
-# points = points.cpu().numpy()
-# preds = preds.cpu().numpy()
-# new_space = new_space.cpu().numpy()
+points = points.cpu().numpy()
+preds = preds.cpu().numpy()
+new_space = new_space.cpu().numpy()
 
-# fig = plt.figure(figsize=(20, 20))
-# ax1 = fig.add_subplot(1, 2, 1, projection="3d")
-# ax2 = fig.add_subplot(1, 2, 2, projection="3d")
-# ax1.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=preds.squeeze())
-# ax2.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=new_space.squeeze())
-# plt.show()
+fig = plt.figure(figsize=(20, 20))
+ax1 = fig.add_subplot(1, 2, 1, projection="3d")
+ax2 = fig.add_subplot(1, 2, 2, projection="3d")
+ax1.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=preds.squeeze())
+ax2.scatter(points[:, :, 0], points[:, :, 1], points[:, :, 2], c=new_space.squeeze())
+plt.show()
